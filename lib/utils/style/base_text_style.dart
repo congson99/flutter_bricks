ade4fc470b5647759945a948b0a0be27d48b3d42
Công Sơn <48781511+congson99@users.noreply.github.com>
2023-02-20 01:53:50 +0700

    [Style] Add Package Info Page (#2)

--- lib/utils/style/base_text_style.dart
+++ lib/utils/style/base_text_style.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
 
-import 'base_color.dart';
-
 class BaseTextStyle {
   static TextStyle heading({Color? color, double? fontSize}) {
     return TextStyle(
         fontSize: fontSize ?? 32,
         fontWeight: FontWeight.w600,
-        color: color ?? BaseColor.grey900);
+        color: color ?? Colors.black);
   }
 
   static TextStyle subtitle1({Color? color}) {
     return TextStyle(
         fontSize: 20,
         fontWeight: FontWeight.w600,
-        color: color ?? BaseColor.grey900);
+        color: color ?? Colors.black);
   }
 
   static TextStyle subtitle2({Color? color}) {
     return TextStyle(
         fontSize: 18,
         fontWeight: FontWeight.w600,
-        color: color ?? BaseColor.grey900);
+        color: color ?? Colors.black);
   }
 
   static TextStyle label({Color? color}) {
     return TextStyle(
         fontSize: 16,
         fontWeight: FontWeight.w500,
-        color: color ?? BaseColor.grey900);
+        color: color ?? Colors.black);
   }
 
   static TextStyle body({Color? color}) {
-    return TextStyle(fontSize: 14, color: color ?? BaseColor.grey900);
+    return TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: color ?? Colors.black);
   }
 
   static TextStyle caption({Color? color, double? fontSize}) {
-    return TextStyle(fontSize: 12, color: color ?? BaseColor.grey900);
+    return TextStyle(fontSize: 12, color: color ?? Colors.black);
   }
 }
--- lib/utils/style/base_theme.dart
+++ lib/utils/style/base_theme.dart
@@ -1,11 +1,10 @@
+import 'package:bricks/utils/helper/color_helper.dart';
 import 'package:flutter/material.dart';
-
-import 'base_color.dart';
 
 ThemeData baseTheme() {
   final ThemeData base = ThemeData(
-    primaryColor: BaseColor.green600,
-    scaffoldBackgroundColor: Colors.white,
-  );
+      primaryColor: Colors.black,
+      scaffoldBackgroundColor: Colors.white,
+      primarySwatch: ColorHelper.createMaterialColor(Colors.white));
   return base;
 }
