a6f0363d8fcfc4edeb528d82689f301f38e53d69
Cong Son <congson99vn@gmail.com>
2023-02-20 00:40:47 +0700

    f

--- lib/utils/style/base_theme.dart
+++ lib/utils/style/base_theme.dart
@@ -1,11 +1,12 @@
+import 'package:bricks/utils/helper/color_helper.dart';
 import 'package:flutter/material.dart';
 
 import 'base_color.dart';
 
 ThemeData baseTheme() {
   final ThemeData base = ThemeData(
-    primaryColor: BaseColor.green600,
-    scaffoldBackgroundColor: Colors.white,
-  );
+      primaryColor: BaseColor.green600,
+      scaffoldBackgroundColor: Colors.white,
+      primarySwatch: ColorHelper.createMaterialColor(Colors.white));
   return base;
 }
