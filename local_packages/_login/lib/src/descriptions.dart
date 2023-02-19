fc94465b1b5d9468ba4088394131bb5d509fbc70
Cong Son <congson99vn@gmail.com>
2023-02-19 22:10:28 +0700

    X

--- local_packages/_login/lib/src/descriptions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:_login/src/presentations/demo_page.dart';
-import 'package:flutter/material.dart';
-
-class FilePackageDescriptions {
-  final String version = "1.0.0";
-  final String name = "Login";
-  final Widget demoPage = const LoginPackageDemoPage();
-  final String description = "Login";
-}
