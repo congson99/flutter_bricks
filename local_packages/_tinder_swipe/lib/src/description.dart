0146b911885e37aaa27c0a9f109f0c337813b1f6
Cong Son <congson99vn@gmail.com>
2023-02-22 13:45:54 +0700

    init

---
+++ local_packages/_tinder_swipe/lib/src/description.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'presentations/demo_page.dart';
+
+class TinderSwipePackageDescription {
+  final String version = "1.0.0";
+  final String name = "File";
+  final Widget demoPage = const TinderSwipePackageDemoPage();
+  final String description = "Provides some functionality with files."
+      "\n\n"
+      "Includes: pick, display card with icon, get match icon."
+      "\n\n"
+      "Dependencies:";
+}
---
+++ local_packages/_tinder_swipe/lib/src/presentations/demo_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TinderSwipePackageDemoPage extends StatelessWidget {
+  const TinderSwipePackageDemoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tinder Swipe"),
+      ),
+      body: const Center(
+        child: Text("Demo Page"),
+      ),
+    );
+  }
+}
