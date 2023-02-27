4bc99fab5536d71427e450d21184c2dd03d69a5c
Cong Son <congson99vn@gmail.com>
2023-02-27 11:42:12 +0700

    done

--- local_packages/_file/lib/src/i_features.dart
+++ local_packages/_file/lib/src/i_features.dart
@@ -4,10 +4,9 @@ import 'package:flutter/cupertino.dart';
 abstract class FilePackageInterface {
   Future<List<PlatformFile>> pickFile();
 
-  Widget fileCard(
-      {required PlatformFile file,
-      double iconSize,
-      BoxDecoration? boxDecoration});
+  Widget fileCard({required PlatformFile file,
+    double iconSize,
+    BoxDecoration? boxDecoration});
 
   Widget fileIcon(PlatformFile file, double? size);
 
