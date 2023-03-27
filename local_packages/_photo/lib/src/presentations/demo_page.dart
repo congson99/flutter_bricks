b1187b3551771c1b12ad7e79d5d02e12cfeb0b4d
Cong Son <congson99vn@gmail.com>
2023-03-27 23:58:28 +0700

    done

--- local_packages/_photo/lib/src/presentations/demo_page.dart
+++ local_packages/_photo/lib/src/presentations/demo_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
 class PhotoPackageDemoPage extends StatelessWidget {
   const PhotoPackageDemoPage({Key? key}) : super(key: key);
